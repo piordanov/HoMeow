0e9ceaa7ee1f2aaf748b72c0dd6feabb4b6b6f45
Shleo <leohlim@gmail.com>
2019-01-26 12:26:32 -0600

    Added script that allows camera to follow player
    Camera has an orthographic view and has adjustable smoothing (slight lag when following player)

---
+++ HomeMeow/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;  // Position that camera will follow
+    public float smoothing = 5f;  // The smoothness of camera movement
+
+    Vector3 offset;             //Initial offset #endregion
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate ()
+    {
+        // Create camera position based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp (transform.position, targetCamPos,smoothing * Time.deltaTime);
+    }
+
+}
