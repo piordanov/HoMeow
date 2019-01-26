a5ad9deb029517e33bded2b27f7dc46fa1279d35
Shleo <leohlim@gmail.com>
2019-01-26 17:50:00 -0600

    Added scripts for taking feral enemy damage

---
+++ HomeMeow/Assets/Scripts/FeralAttack.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeralAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;         // Seconds between attack
+    public int attackDamage = 1;                    // Amount of damage deducted from total health per attack
+
+
+    Animator anim;
+    GameObject player;
+    FeralHealth feralHealth;                        // Reference to feral cat health
+    bool  playerInRange;                            // Whether player is in range with trigger collider
+    float timer;                                    // Timer that counts time before next attack
+
+
+    // Start is called before the first frame update
+    void Awake ()
+    {
+        //Set up references
+        player = GameObject.FindGameObjectWithTag ("Player");
+        PlayerHealth = player.GetComponent <PlayerHealth> ();
+        feralHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator>;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        //If the entering collider is the player...
+        if(other.gameObject == player)
+        {
+            //... player is in range
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        // If the exiting collider is the player...
+        if(other.gameObject == player)
+        {
+            //...player is no longer in range
+            playerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Add time since Update was called to timer
+        timer += time.deltaTime;
+
+        //If the timer exceeds the time between attacks, player is in range, and feral cat is still alive...
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            //... attack.
+            Attack ();
+        }
+
+        // if the player has zero or less health
+
+        if(PlayerHealth.currentHealth <= 0)
+        {
+            //...tell animator player is dead
+            anim.SetTrigger ("PlayerDead");
+        }
+        
+        void Attack ()
+        {
+            //Reset timer
+            timer = 0f;
+
+            //If player has health to lose...
+
+            if(PlayerHealth.currentHealth > 0)
+            {
+                //...damage the player.
+                PlayerHealth.TakeDamage (attackDamage);
+            }
+        }
+    }
+}
---
+++ HomeMeow/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;        //Total starting health
+    public int currentHealth;               //Current amount of player health
+    public Slider healthSlider;             //References the UI image of health
+    public Image damageImage;               //Image that flashes on screen when taking damage
+
+    public AudioClip deathClip;             //Audio clip that plays when player dies
+    public float flashSpeed = 5f;           //The speed that the damageImage will fade
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f); //The colour the damageImage will flash
+
+    public Animator anim;                          //Reference to animator component
+    public AudioSource playerAudio;                //References AudioSource component.
+    public PlayerController playerController;          //References player's movement
+    bool isDead;                            //Whether player is dead.
+    bool damaged;                           //True when player gets damaged.
+
+    // Start is called before the first frame update
+    void Awake ()
+    {
+        //Reference setup
+
+        anim = GetComponent <Animator>();
+        playerAudio = GetComponent <AudioSource>();
+        playerController = GetComponent <PlayerController>();
+
+        //Set initial health
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If player takes damage
+        if(damaged)
+        {
+            //...set color of damageImage to flash color
+            damageImage.color = flashColour;
+        }
+        //Otherwise...
+        else
+        {
+            //...transition color back to clear
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        //Reset the damaged flag.
+
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        //Set the damaged flag so screen will flash
+        damaged = true;
+
+        //Reduce current health by damage amount
+        currentHealth -= amount;
+
+        //Set the health bar's value to the current health. 
+        healthSlider.value = currentHealth;
+
+        //Play hurt sound effect
+        playerAudio.Play ();
+
+        //If the player has lost all it's health and the death flag hasn't been set
+        if(currentHealth <= 0 && !isDead)
+        {
+            //then kill player
+            Death();
+        }
+    }
+
+    void Death ()
+    {
+        //Set the death flag so this function won't be recalled
+        isDead = true;
+
+        //Turn off any shooting effects
+
+        //Tell the animator that the player is dead.
+        anim.SetTrigger ("Die");
+
+        //Set the audiosource to play the death clip and play it (this will stop hurt sound)
+        playerAudio.clip = deathClip;
+        playerAudio.Play ();
+
+        //Turn off movement and shooting
+        playerController.enabled = false;
+    }
+}
