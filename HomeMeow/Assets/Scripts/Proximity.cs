49d3176818f401d15b2563c6b9e76aea834fac52
Anna <annashabayev@gmail.com>
2019-01-26 11:38:08 -0600

    started stuff with reactive behavior

---
+++ HomeMeow/Assets/Scripts/Proximity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proximity : MonoBehaviour
+{
+
+    EnemyBehavior myParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myParent = transform.parent.GetComponent<EnemyBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            myParent.runAway = true;
+            Debug.Log("runAway");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            myParent.runAway = false;
+            Debug.Log("you good");
+        }
+    }
+
+}
