2bc0702b2e92ebd82f35c347c6fa8da5aad0faec
Peter Iordanov <peter@bulbyte.com>
2019-01-26 14:19:44 -0600

    base inventory control

---
+++ HomeMeow/Assets/Scripts/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemController : MonoBehaviour
+{
+    protected Rigidbody rgb;
+
+    public void Start() {
+        rgb = GetComponent<Rigidbody> ();
+        OnStart();
+    }
+
+    virtual public void OnStart() {
+
+    }
+
+    public bool isThrowable() {
+        return true;
+    }
+
+}
---
+++ HomeMeow/Assets/Scripts/TunaCanController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunaCanController : ItemController
+{
+    // Start is called before the first frame update
+    public override void OnStart()
+    {
+        rgb.velocity = new Vector3(10, 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
