adfc22c7ac147fc77ce258084246b603d5b8602d
Anna <annashabayev@gmail.com>
2019-01-26 11:19:35 -0600

    Basic Player Controller for testing NPC behavior

---
+++ HomeMeow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementspeed = 1f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementspeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movementspeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+
+}
