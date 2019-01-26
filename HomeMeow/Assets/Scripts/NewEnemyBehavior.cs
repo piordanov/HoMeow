2822cfa6a0f35e3fc91dd17c79b6240e4a1468ad
Lucas Perez <knightfication@gmail.com>
2019-01-26 16:32:16 -0600

    Updating NewEnemyBehavior

--- HomeMeow/Assets/Scripts/NewEnemyBehavior.cs
+++ HomeMeow/Assets/Scripts/NewEnemyBehavior.cs
@@ -6,6 +6,10 @@ public class NewEnemyBehavior : MonoBehaviour
 {
     private GameObject player;
     public float movementSpeed = 5;
+    private bool attack;
+    private bool isPlayerInBlob;
+    private int distanceFromPlayer = ; // Needs to be set
+    private int threshold = 50; // Minimum distance from when it starts being checked
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +24,36 @@ public class NewEnemyBehavior : MonoBehaviour
         transform.position += transform.forward * movementSpeed * Time.deltaTime;
     }
 
-    private void Behavior()
+    private bool Chase()
     {
-        player = player.transform.position;
+        if (isPlayerInBlob)
+        {
+            // write do chase
+            return true;
+            // player.transform.position
+        }
+        else if (distanceFromPlayer < threshold) // If distanceFromPlayer is smaller, it keeps running randomNumberGen() until it hits 0, 10% chance
+        {
+            randomNumberGen();
+        }
+    }
+
+    private bool randomNumberGen()
+    {
+        int min = 0;
+        int max = 10; // Upper bound not included on Next() according to C# docs
+        // https://docs.microsoft.com/en-us/dotnet/api/system.random.next?view=netframework-4.7.2
+
+        Random random = new Random();
+        int n = random.Next(min, max);
+
+        if (n < 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
