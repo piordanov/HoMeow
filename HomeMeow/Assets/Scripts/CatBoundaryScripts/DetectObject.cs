4cf2135e85c518d1883e4a444283ed468a1c3e61
Anna <annashabayev@gmail.com>
2019-01-26 18:01:26 -0600

    Cat behavior to different things
    behave to enemies, to player getting attacked, to finding object

---
+++ HomeMeow/Assets/Scripts/CatBoundaryScripts/DetectObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObject : MonoBehaviour
+{
+    CatBehavior myParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myParent = transform.parent.GetComponent<CatBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Repair Kit"))
+        {
+            myParent.nearbyKit = true;
+            myParent.nearestRepairKit = other.gameObject;
+            Debug.Log("Found repair kit");
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            myParent.playerLeft = false;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Repair Kit"))
+        {
+            myParent.nearbyKit = false;
+            myParent.nearestRepairKit = other.gameObject;
+            Debug.Log("Out Boundary for repair kit");
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            myParent.playerLeft = true;
+        }
+    }
+}
---
+++ HomeMeow/Assets/Scripts/CatBoundaryScripts/EnemyDetection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    CatBehavior myParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myParent = transform.parent.GetComponent<CatBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            myParent.enemyDetected = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            myParent.enemyDetected = false;
+        }
+    }
+}
