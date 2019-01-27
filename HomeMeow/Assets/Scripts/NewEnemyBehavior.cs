82cafa23a1161b027d2b8d47c3af7de267085ab5
Lucas Perez <knightfication@gmail.com>
2019-01-26 22:58:20 -0600

    NewEnemyBehavior.cs

--- HomeMeow/Assets/Scripts/NewEnemyBehavior.cs
+++ HomeMeow/Assets/Scripts/NewEnemyBehavior.cs
@@ -5,64 +5,28 @@ using UnityEngine;
 public class NewEnemyBehavior : MonoBehaviour
 {
     private GameObject player;
-    public float movementSpeed = 5;
-    private bool attack;
-    private bool isPlayerInBlob = false;
-    private int distanceFromPlayer = 25; // Needs to be set
-    private int threshold = 50; // Minimum distance from when it starts being checked
+    public int moveSpeed = 5;
+    private int distanceFromPlayer = 1;
+    private int threshold = 25; // Maximum distance, technically. *Nicky Young gif*
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
-        randomNumberGen();
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform);
-        transform.position += transform.forward * movementSpeed * Time.deltaTime;
         Chase();
     }
 
-    private bool Chase()
+    private void Chase()
     {
-        if (isPlayerInBlob)
+        if (distanceFromPlayer < threshold)
         {
-            Vector3 p = player.transform.position;
-
-            // DO CHASE
-            return true;
-        }
-        else if (distanceFromPlayer < threshold) // If distanceFromPlayer is smaller, it keeps running randomNumberGen() until it hits 0, 10% chance
-        {
-            isPlayerInBlob = true;
-            randomNumberGen();
-        }
-        else
-        {
-            return false;
-        }
-        return false;
-    }
-
-    private bool randomNumberGen()
-    {
-        int min = 0;
-        int max = 10; // Upper bound exclusive
-
-        int n = Random.Range(min, max);
-
-        if (n < 1)
-        {
-            print("Random is " + n);
-            return true;
-        }
-        else
-        {
-            print("Random is " + n);
-            return false;
+            transform.LookAt(player.transform);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
     }
 }
