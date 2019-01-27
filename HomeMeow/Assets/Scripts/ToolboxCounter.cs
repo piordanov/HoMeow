4a2c5cf0c1c801497670995bd95bb4e38e0c784d
Lucas Perez <knightfication@gmail.com>
2019-01-26 22:12:05 -0600

    Is this real life?

---
+++ HomeMeow/Assets/Scripts/ToolboxCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ToolboxCounter : MonoBehaviour
+{
+    private int count;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        count = 0;
+        OnGUI();
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+    }
+}
